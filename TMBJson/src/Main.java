56df8c8643897409a2d466a8696957e0c4f82a26
Eric Macià <ericmaciared@gmail.com>
2019-12-18 20:29:56 +0100

    Update de classes varies again

--- TMBJson/src/Main.java
+++ TMBJson/src/Main.java
@@ -1,5 +1,8 @@
+import DataModel.User;
 import Utils.Logic;
 import Utils.Menu;
+
+import java.io.FileNotFoundException;
 
 /**
  * Main class.
@@ -12,15 +15,27 @@ import Utils.Menu;
 public class Main {
 
     public static void main(String[] args) {
+        User user;
         Logic logic = new Logic();
         Menu menu = new Menu();
 
+        //First we import the localizations
+        try{
+            logic.loadLocations();
+        } catch (FileNotFoundException e) {
+            System.out.println("\nERROR: There was a problem when reading the Locations file.");
+            return;
+        }
+
+        //Login of user
+        //user = menu.login();
+
         do {
-            menu.printMainMenu();
             do {
+                menu.printMenu();
                 menu.askForOption();
             } while (!menu.validOption());
-        } while(!menu.exit());
 
+        } while(!menu.exit());
     }
 }
